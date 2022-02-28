8bc74c7d2f2ae1093e1d40704f57b7554e67db30
vanhuycx <vanhuycx@gmail.com>
2022-02-27 22:59:31 -0500

    Add script to document body and call the widget component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React,{ useState,useEffect } from 'react'
 
+
 const App = () => {
+  const  element = document.createElement('script') 
+  
+  element.src='https://widgets.coingecko.com/coingecko-coin-ticker-widget.js'
+  element.async = true;
+  document.body.appendChild(element)
+
 
   return (
     <>
     <h1>Fear and Greed index by Chart JS </h1>
+    <p>Bitcoin</p>
+
+
+    <coingecko-coin-ticker-widget  coin-id="bitcoin" currency="usd" locale="en"></coingecko-coin-ticker-widget> 
+
     </>
 
   ) 
