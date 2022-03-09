cbb72faf00bb3b1d53d16f9f1c78301e197992ba
vanhuycx <vanhuycx@gmail.com>
2022-03-09 17:49:14 -0500

    Add layout for cocktail collection

--- script.js
+++ script.js
@@ -1,34 +1,56 @@
+const getCockTailData = async () => {
+    const url = 'https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Cocktail'
 
-    $(document).ready(function(){
-    
-        $.fn.showTooltip = function (options) {
-            this.hover(()=> {
-                // Set the mouseenter effect
-                const title = document.createElement('h5')
-                title.innerHTML=options.title
-
-                const description = document.createElement('p')
-                description.innerHTML=options.description
-
-                const div = document.createElement('div')
-                div.appendChild(title)
-                div.appendChild(description)
+    try {
+        const allDrinks = await fetch(url)
+                .then(response => response.json())
+                .then(resposne=>resposne.drinks)
+        return allDrinks
+        
+    } catch (error) {
+        console.log(error)
+    }
+}
 
 
+const renderData = async () => {
+    const allDrinks = await  getCockTailData()
+    let html = ''
 
-                document.body.appendChild(div)
-            },
-            // Leave the mouseleave trigger as blank
-            ()=>{})
-        };
+    console.log(allDrinks)
+
+    allDrinks.forEach((drink) => {
+        let htmlPortion = `
+
+                        <div class="cocktail">
+                            <h3>${drink.strDrink}</h3>
+                            <img class='cocktail-img' src=${drink.strDrinkThumb} alt="">
+                        </div>
+                            `
+        html += htmlPortion
+    })
+    const cocktailContainer = document.querySelectorAll('.cocktail-container')[0]
+    cocktailContainer.innerHTML = html
+}
+
+renderData()
 
 
-        $('.tooltip1').showTooltip({
-            title:'Title',
-            description:'Tooltip shows description'
-
-        })
 
 
- 
-    });
+
+
+
+
+// const getJSONData = async () => {
+//     const url = 'https://jsonplaceholder.typicode.com/todos/'
+
+//     try {
+//         const data = await fetch(url)
+//                 .then(response => response.json())
+//         return data
+        
+//     } catch (error) {
+//         console.log(error)
+//     }
+// }
