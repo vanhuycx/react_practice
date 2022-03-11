c9d103444e78957e89c9c3b348fb5b1deb48d089
vanhuycx <vanhuycx@gmail.com>
2022-03-11 12:14:04 -0500

    ParseInt ParseFloat

--- script.js
+++ script.js
@@ -1,56 +1,22 @@
-class NotesStore {
-    constructor(state,name) {
-        this.state = state;
-        this.name = name;
-        this.notes = {}
-    }
-    addNote(state,name) {
-        const accepted_states = ['completed','active','others']
-        try {
-            if (!name) {
-                return ('Error: Name cannot be empty')
-            }
-            if (accepted_states.includes(state)) {
-                if (!(state in this.notes)){
-                    this.notes[state] = [name]
-                } else {
-                    this.notes[state].push(name)  
-                }             
-            } else {
-                return ('Error: Invalid state '+ state)
-                }
-        } catch(e) {
-            return e
-        }
-    }
-    
-    getNotes(state) {
-        const accepted_states = ['completed','active','others']
-        try {
-            if (accepted_states.includes(state)) {
-                if (!(state in this.notes) || this.notes[state].length===0) {
-                    return ('No Notes')   
-                }  else {
-                    return this.notes[state]
-                }
-            } else {
-                return ('Error: Invalid state '+ state)
-            }
-        } catch(e) {
-            return e
-        }
-    }
+function main() {
+    var i = 4
+    var d = 4.0
+    var s = "HackerRank "
+    // Declare second integer, double, and String variables.
+    var secondI,secondD,secondS
+
+    // Read and save an integer, double, and String to your variables.
+    secondI = 4
+    secondD = 4.0
+    secondS = 'Here!'
+
+    // Print the sum of both integer variables on a new line.
+    console.log(i +parseInt(secondI))
+    // Print the sum of the double variables on a new line.
+    console.log(parseFloat(d + secondD).toFixed(1))
+    // Concatenate and print the String variables on a new line
+    // The 's' variable above should be printed first.
+    console.log(s+secondS)
 }
 
-const notes =  new NotesStore()
-
-notes.addNote('completed','Math test')
-console.log(notes.getNotes('completed'))
-notes.addNote('completed','Bio')
-console.log(notes.getNotes('completed'))
-notes.addNote('active','Chemistry')
-
-
-console.log(notes.addNote('others',''))
-console.log(notes.addNote('dawd','daw'))
-console.log(notes.getNotes('activ'))
+main()
