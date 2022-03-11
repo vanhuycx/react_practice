097ad5a9c3603793b0f9953dc66c45c8320ea45d
vanhuycx <vanhuycx@gmail.com>
2022-03-10 19:35:01 -0500

    Create a NoteStore class with addNote and getNotes functions

--- script.js
+++ script.js
@@ -1,75 +1,56 @@
-const getCockTailData = async () => {
-    const url = 'https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Cocktail'
-
-    try {
-        const allDrinks = await fetch(url)
-                .then(response => response.json())
-                .then(resposne=>resposne.drinks)
-        return allDrinks
-        
-    } catch (error) {
-        console.log(error)
+class NotesStore {
+    constructor(state,name) {
+        this.state = state;
+        this.name = name;
+        this.notes = {}
+    }
+    addNote(state,name) {
+        const accepted_states = ['completed','active','others']
+        try {
+            if (!name) {
+                return ('Error: Name cannot be empty')
+            }
+            if (accepted_states.includes(state)) {
+                if (!(state in this.notes)){
+                    this.notes[state] = [name]
+                } else {
+                    this.notes[state].push(name)  
+                }             
+            } else {
+                return ('Error: Invalid state '+ state)
+                }
+        } catch(e) {
+            return e
+        }
+    }
+    
+    getNotes(state) {
+        const accepted_states = ['completed','active','others']
+        try {
+            if (accepted_states.includes(state)) {
+                if (!(state in this.notes) || this.notes[state].length===0) {
+                    return ('No Notes')   
+                }  else {
+                    return this.notes[state]
+                }
+            } else {
+                return ('Error: Invalid state '+ state)
+            }
+        } catch(e) {
+            return e
+        }
     }
 }
 
+const notes =  new NotesStore()
 
-const renderData = async () => {
-    let allDrinks = await  getCockTailData()
-    let html = ''
-
-    allDrinks.forEach((drink) => {
-        let htmlPortion = `
-
-                        <div class="cocktail">
-                            <h3>${drink.strDrink}</h3>
-                            <img class='cocktail-img' src=${drink.strDrinkThumb} alt="">
-                        </div>
-                            `
-        html += htmlPortion
-    })
-    const cocktailContainer = document.querySelectorAll('.cocktail-container')[0]
-    cocktailContainer.innerHTML = html
-
-    
-
-    const searchBar = document.querySelectorAll('.search-bar')[0]
-    searchBar.addEventListener('input',(event)=>{
-        let html =''
-        let fileter_drinks = allDrinks.filter((drink) => {
-            return (drink.strDrink.toLowerCase().includes(event.target.value.trim())
-                || drink?.strDrink?.toLowerCase().trim().startsWith(event.target.value.trim()))
-        })
-
-        fileter_drinks.forEach((drink) => {
-            let htmlPortion = `
-                            <div class="cocktail">
-                                <h3>${drink.strDrink}</h3>
-                                <img class='cocktail-img' src=${drink.strDrinkThumb} alt="">
-                            </div>
-                                `
-            html += htmlPortion
-        })
-        cocktailContainer.innerHTML = html
-    })
-}
-
-renderData()
+notes.addNote('completed','Math test')
+console.log(notes.getNotes('completed'))
+notes.addNote('completed','Bio')
+console.log(notes.getNotes('completed'))
+notes.addNote('active','Chemistry')
 
 
-
-
-
-
-
-// const getJSONData = async () => {
-//     const url = 'https://jsonplaceholder.typicode.com/todos/'
-
-//     try {
-//         const data = await fetch(url)
-//                 .then(response => response.json())
-//         return data
-        
-//     } catch (error) {
-//         console.log(error)
-//     }
-// }
+console.log(notes.addNote('others',''))
+console.log(notes.addNote('dawd','daw'))
+console.log(notes.getNotes('activ'))
