b4a4bcbd89313da1c4933b6c0664aaa79713e05f
vanhuycx <vanhuycx@gmail.com>
2022-03-11 18:40:34 -0500

    Basic layout for Kaban board

--- src/App.js
+++ src/App.js
@@ -2,68 +2,70 @@ import React,{ useState,useEffect } from 'react'
 import './App.css'
 import { slides } from "./data/data";
 
+
 const App = () => {
-  const [index, setIndex] = useState(0)
+  const [taskName, setTaskName] = useState('Task1')
 
-  const restartIndex = () => {
-      setIndex(0)
-    }
-  const decreaseIndex = () => {
-      setIndex(index=>index-1)
+
+  const Card = () => {
+    return  (
+      <>
+      <div className="card">
+        <div className="left-section">
+          {taskName}
+        </div>
+        <div className="right-section">
+          <button><i class='fas'>&#xf104;</i></button>
+          <button><i class='fas'>&#xf105;</i></button>
+          <button><i class="fa">&#xf014;</i></button>
+        </div>
+      </div>
+      </>
+    )
   }
-  const increaseIndex = () => {
-      setIndex(index=>index+1)
-  }
-  const buttons = document.getElementsByTagName('button')
-
-  // console.log(buttons.length)
-  useEffect(() => {
-   
-
-    if (index===slides.length-1) {
-      document.getElementById('next-button').setAttribute('disabled',true)
-    } else{
-      document.getElementById('next-button').removeAttribute('disabled')
-    }
-
-    if (index===0) {
-      document.getElementById('previous-button').setAttribute('disabled',true)
-      document.getElementById('restart-button').setAttribute('disabled',true)
-    } else{
-      document.getElementById('previous-button').removeAttribute('disabled')
-      document.getElementById('restart-button').removeAttribute('disabled')
-    }
-
-    for (let i=0;i<buttons.length;i++){
-      if (!buttons[i].disabled) {
-        buttons[i].style.color='black'
-      } else {
-        buttons[i].style  .color='white'
-      }
-    }
-
-
-  }, [index,buttons])
-
 
   return (
     <>
     <div className="site-wrapper">
+        <h1>Kaban Board</h1>
+
+
       <div className="main-content">
-        <div className="buttons">
-          <button id='restart-button' onClick={()=>restartIndex()}>Restart</button>
-          <button id='previous-button' onClick={()=>decreaseIndex()}>Previous</button>
-          <button id='next-button' onClick={()=>increaseIndex()}>Next</button>
+        <div className="new-task">
+          <input type="text" />
+          <button>Create Task</button>
         </div>
-        <div className="slide-show">
-          <h1>{slides[index].title}</h1>
-          <h3>{slides[index].text}</h3>
+
+
+        <div className="stages">
+          <div>
+            <h3>Backlog</h3>
+            <Card/>
+          </div>
+
+          <div>
+            <h3>To Do</h3>
+            <Card/>
+          </div>
+
+
+          <div>
+            <h3>Ongoing</h3>
+            <Card/>
+          </div>
+
+          <div>
+            <h3>Done</h3>
+            <Card/>
+          </div>
+
         </div>
       </div>
-      
+
 
     </div>
-   
+    
+    
     </>
 
   ) 
