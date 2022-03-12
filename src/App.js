3cf6961d01cd0bf88d4c712cc9b7a3eb7d6ae7b7
vanhuycx <vanhuycx@gmail.com>
2022-03-12 14:35:22 -0500

    Implement responsive inputs for celsius and fahrenheit conversion

--- src/App.js
+++ src/App.js
@@ -5,98 +5,37 @@ import { backLog } from "./data/data";
 
 
 const App = () => {
-  const [backLogTask, setBackLogTask] = useState([])
-  const [index, setIndex] = useState(0)
-  const [taskName, setTaskName] = useState()
+  const [celsius, setCelsius] = useState(0)
+  const [fahrenheit, setFahrenheit] = useState(0)
+  
+  useEffect(() => {
+    // if (celsius) {
+    //   setFahrenheit(0)
+    // }
+    // if (!fahrenheit) {
+    //   setCelsius(0)
+    // }
 
-  // setAllTaskNames(taskNames)
-  // console.log({allTaskNames})
-  const removeCard = ({id,array}) => {
-    setBackLogTask(array.filter(item=>item['id']!==id))
-    
-  }
+    console.log(typeof(celsius))
 
-  const moveCardRight = () => {
-
-  }
-
-
-  const Card = ({name,id,array}) => {
-    return  (
-      <>
-      <div className="card">
-        <div className="left-section">
-          {name}
-        </div>
-        <div className="right-section">
-          <button><i class='fas'>&#xf104;</i></button>
-          <button onClick={()=>moveCardRight({id,array})}><i class='fas'>&#xf105;</i></button>
-          <button onClick={()=>removeCard({id,array})}><i class="fa">&#xf014;</i></button>
-        </div>
-      </div>
-      </>
-    )
-  }
-
+  }, [celsius])
   
 
+console.log(celsius)
   return (
     <>
-    <div className="site-wrapper">
-        <h1>Kaban Board</h1>
+    <h1>Celcius</h1>
+    <input id='celsius' value={celsius}  onChange={(e)=>setCelsius(e.target.value)} type="text" />
+
+{/* onChange={(e)=>setFahrenheit(e.target.value)} */}
+    <h1>Fahrenheit</h1>
+    <input id='fahrenheit' value={fahrenheit} onChange={(e)=>setFahrenheit(e.target.value)}  type="text" />
 
 
-      <div className="main-content">
-        <div className="new-task">
-          <input type="text" placeholder="Enter new task name" onChange={(e)=>setTaskName(e.target.value)}/>
-          <button onClick={()=>{
-            taskName&&
-            setBackLogTask((backLogTask)=>[...backLogTask,{'id':index,'name':taskName}])
-            setIndex((index)=>index+1)
-          }
-          }>Create Task</button>
-
-          {console.log({backLogTask,index})}
-        </div>
-
-       
+      
 
 
-        <div className="stages">
-          <div id='backlog' className="stage">
-            <h2>Backlog</h2>
-            {backLogTask.map((item,index,array)=>(
-              item &&
-              <Card name={item.name} id={item.id} array={array} />
-            ))}
-
-          </div>
-
-          <div className="stage">
-            <h2>To Do</h2>
-
-          </div>
-
-
-          <div className="stage">
-            <h2>Ongoing</h2>
-
-          </div>
-
-          <div className="stage">
-            <h2>Done</h2>
-
-          </div>
-
-        </div>
-      </div>
-
-
-    </div>
-    
-    
     </>
-
   ) 
 };
 
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{useState} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
@@ -29,17 +29,60 @@ ChartJS.register(
   Legend
 );
 
+const Calculate = () => {
+  const [first, setfirst] = useState(0)
+
+  return (
+    <>
+    { 
+   <script src="https://cointelegraph.com/news-widget" data-ct-widget-limit="5" data-ct-widget-theme="dark" data-ct-widget-size="small" data-ct-widget-language="en"></script>
+}
+
+    
+    </>
+  )
+
+}
+
+
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <Provider store={store}>
       <App />
+      {/* <Calculate/> */}
       </Provider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
 
+
+
+const celInput = document.getElementById('celsius')
+const fahInput = document.getElementById('fahrenheit')
+
+      
+celInput.addEventListener('keyup',()=>{
+  if (celInput.value) {
+    fahInput.value = celInput.value*(9/5)+32
+  } else {
+    fahInput.value =''
+  }
+})
+
+fahInput.addEventListener('keyup',()=>{
+  if (fahInput.value) {
+  celInput.value = (fahInput.value - 32)*5/9
+  } else {
+    celInput.value =''
+  }
+
+})
+
+
+      
+  
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
