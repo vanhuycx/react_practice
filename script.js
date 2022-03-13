1a057f87306dbf71135d34f6f3a70252beca4f1e
vanhuycx <vanhuycx@gmail.com>
2022-03-13 17:50:42 -0400

    Test Object.entries, Object.values, and Object.from Entries

--- script.js
+++ script.js
@@ -1,22 +1,8 @@
-function main() {
-    var i = 4
-    var d = 4.0
-    var s = "HackerRank "
-    // Declare second integer, double, and String variables.
-    var secondI,secondD,secondS
+let obj = {"you": 100, "me": 75, "foo": 116, "bar": 15};
 
-    // Read and save an integer, double, and String to your variables.
-    secondI = 4
-    secondD = 4.0
-    secondS = 'Here!'
 
-    // Print the sum of both integer variables on a new line.
-    console.log(i +parseInt(secondI))
-    // Print the sum of the double variables on a new line.
-    console.log(parseFloat(d + secondD).toFixed(1))
-    // Concatenate and print the String variables on a new line
-    // The 's' variable above should be printed first.
-    console.log(s+secondS)
-}
+const entries = Object.entries(obj).sort((a,b)=>b[1]-a[1])
 
-main()
+
+
+console.log(Object.fromEntries(entries),obj)
