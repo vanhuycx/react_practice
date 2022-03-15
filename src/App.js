1744c126824604b0474458b1fa34ac1bea91ba46
vanhuycx <vanhuycx@gmail.com>
2022-03-15 14:34:56 -0400

    Test toggle Hamburger menu

--- src/App.js
+++ src/App.js
@@ -3,9 +3,28 @@ import './App.css'
 
 const App = () => {
 
+  const myFunction = (x)=> {
+    document.getElementsByClassName('container')[0].classList.toggle("change")
+  }
+
   return (
     <>
+    <div className="overlay">
+        <div className="content">
+          Hi there
+        </div>
+
+      </div>
+
+      <div className="container" onClick={()=>myFunction(this)}>
+  <div className="bar1"></div>
+  <div className="bar2"></div>
+  <div className="bar3"></div>
+      </div>
+
     <div className="web-container">
+      
+
       <div className="nav-container">
 
         <div className="web-name">
@@ -19,8 +38,8 @@ const App = () => {
           <a href="/">Featured</a>
           <a href="/">Topics</a>
           <a href="/">Newsletters</a>
-          <a href="/">Event</a>
-          <a href="/">PodCast</a>
+          <a href="/">Events</a>
+          <a href="/">PodCasts</a>
         </div>
 
         <div className="web-signin">
@@ -33,6 +52,8 @@ const App = () => {
         </div>
 
       </div>
+
+      
 
       <div className="content-container">
         <div className="content">
