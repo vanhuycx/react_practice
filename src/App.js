2090e964726c40e39866a8dfddc05b08e1a7ab4f
vanhuycx <vanhuycx@gmail.com>
2022-03-21 22:01:04 -0400

    Destructuring with comma

--- src/App.js
+++ src/App.js
@@ -4,17 +4,15 @@ import { useGetCryptoQuery } from "./api/cryptoApi";
 
 const App = () => {
   
-  const array1 =  [1,2,3,4]
+  const array1 =  [1,2,3,4,5,6,7]
 
-  const [numbe1,...number2] = array1
+  const [numbe1,,number2,,...number3] = array1
 
-  
-
-
+console.log(number3)
 
   return (
     <>
-  <p>{' ' + number2}</p>
+  <p>{ number3}</p>
 
     </>
   ) 
