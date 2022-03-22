8ea764868369b2fb70f5e97ea8fe46265ddd04a5
vanhuycx <vanhuycx@gmail.com>
2022-03-21 22:51:28 -0400

    Test sass

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
 import React,{ useState,useEffect,useCallback } from 'react'
 import './App.css'
 import { useGetCryptoQuery } from "./api/cryptoApi";
+import './App.scss'
+
+
+let x
 
 const App = () => {
-  
-  const array1 =  [1,2,3,4,5,6,7]
-  const array2 =  [2,15,68,7,89,69,71]
-
-  const  array3 = [...array1]
-
-console.log(array3.sort((a,b)=>b-a))
-array3.push(3)
-console.log(array3)
   return (
     <>
-  <p>{ array3}</p>
-  <p>{ array1}</p>
-
+<h1>Hello world</h1>
 
     </>
   ) 
