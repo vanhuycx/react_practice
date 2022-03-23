d794f0a8d241b9441ba79d23706a792a94b96a4d
vanhuycx <vanhuycx@gmail.com>
2022-03-22 22:37:26 -0400

    Access DOM using useRef

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import React, { useRef, useState, useEffect } from 'react';
 
 const App = () => {
-  const [inputValue, setInputValue] = useState();
-  const count = useRef(0);
+  const inputElement = useRef();
 
-  useEffect(() => {
-    count.current = count.current + 1;
-  });
-
+  const focusInput = () => {
+    inputElement.current.focus();
+  };
   return (
     <>
-      <p>Enter something:</p>
-      <input type='text' onChange={(e) => setInputValue(e.target.value)} />
-      <p>The app renders: {count.current} time(s)</p>
+      <input type='text' ref={inputElement} />
+
+      <button onClick={focusInput}>Focus Input</button>
     </>
   );
 };
