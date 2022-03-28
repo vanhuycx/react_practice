0a97ca93f76cb67e41a91641cf82fc0f3a2d6cf3
vanhuycx <vanhuycx@gmail.com>
2022-03-28 17:21:08 -0400

    Test antd chart

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
 import React, { useRef, useState, useEffect } from 'react';
+import { Line } from '@ant-design/charts';
+import { useGetCryptoQuery } from './api/cryptoApi';
+const data = [
+  { year: '1991', value: 3 },
+  { year: '1992', value: 4 },
+  { year: '1993', value: 3.5 },
+  { year: '1994', value: 5 },
+  { year: '1995', value: 4.9 },
+  { year: '1996', value: 6 },
+  { year: '1997', value: 7 },
+  { year: '1998', value: 9 },
+  { year: '1999', value: 13 },
+];
+
+const config = {
+  data,
+  xField: 'year',
+  yField: 'value',
+  point: {
+    shape: 'diamond',
+  },
+};
 
 const App = () => {
-  const inputElement = useRef();
+  const { data: cryptos } = useGetCryptoQuery();
 
-  const focusInput = () => {
-    inputElement.current.focus();
-  };
+  console.log(cryptos);
   return (
     <>
-      <input type='text' ref={inputElement} />
-
-      <button onClick={focusInput}>Focus Input</button>
+      <Line {...config} />
     </>
   );
 };
